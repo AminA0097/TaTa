d91d8e2ddc3942e5ca6bdce75d0b03c9aa58d841
AminA0097 <springmcserv@gmailc.om>
2025-05-03 08:49:44 +0330

    init

---
+++ src/test/java/com/userservice/tata/TaTaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userservice.tata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaTaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
