4797f15d1ff6172a7893b27508af0bf9b125136f
AminA0097 <springmcserv@gmailc.om>
2025-05-29 07:20:34 +0330

    init

--- src/main/java/com/userservice/tata/Message/MessageController.java
+++ src/main/java/com/userservice/tata/Message/MessageController.java
@@ -1,12 +1,29 @@
 package com.userservice.tata.Message;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/message")
 public class MessageController {
-    private boolean sendMessage(MessageForm messageForm){
+    @PostMapping("/sendmessage")
+    public boolean sendMessage(MessageForm messageForm){
+        return true;
+    }
+    @GetMapping("/getmessage")
+    public List<MessageDto> getMessage(){
+        return null;
+    }
+    @GetMapping("/showdetail")
+    public List<MessageDto> getMessageDetail(String messageId){
+        return null;
+    }
+    @PostMapping("/updatemessage")
+    public boolean updateMessage(MessageForm messageForm){
         return true;
     }
 }
